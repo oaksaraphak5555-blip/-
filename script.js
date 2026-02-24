1d1403f8accfb7634c3d123f6fd293137ca83786
oaksaraphak5555-blip <oaksaraphak5555@gmail.com>
2026-02-24 14:56:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function toggleMenu(){
-document.getElementById("sideMenu").classList.toggle("active");
+  document.getElementById("menu").classList.toggle("active");
+}
+
+function searchStudent(){
+  let input = document.getElementById("searchInput").value.toLowerCase();
+  let rows = document.querySelectorAll("tbody tr");
+
+  rows.forEach(function(row){
+    if(row.textContent.toLowerCase().includes(input)){
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
 }
