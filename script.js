93adfc9bb131e63c7c3921d8f0b36ea985ca1291
oaksaraphak5555-blip <oaksaraphak5555@gmail.com>
2026-03-06 21:19:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,17 @@
 function toggleMenu(){
-document.getElementById("menu").classList.toggle("show");
-}
 
-function sendMsg(){
+let menu=document.getElementById("menu");
 
-let msg=document.getElementById("msg").value;
-let box=document.getElementById("chatBox");
-
-box.innerHTML += "<p>"+msg+"</p>";
-
-document.getElementById("msg").value="";
-}
-
-function searchName(){
-
-let input=document.getElementById("search").value.toLowerCase();
-let table=document.getElementById("studentTable");
-let tr=table.getElementsByTagName("tr");
-
-for(let i=1;i<tr.length;i++){
-
-let td=tr[i].getElementsByTagName("td")[1];
-
-if(td){
-
-let txt=td.textContent;
-
-if(txt.toLowerCase().indexOf(input)>-1){
-
-tr[i].style.display="";
-
+if(menu.style.left==="0px"){
+menu.style.left="-220px";
 }else{
-
-tr[i].style.display="none";
-
+menu.style.left="0px";
 }
 
 }
 
-}
+function toggleDark(){
+
+document.body.classList.toggle("dark");
 
 }
