90f87cb0b2371f4d3830319a200b38afa982604e
oaksaraphak5555-blip <oaksaraphak5555@gmail.com>
2026-03-06 20:21:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,17 +9,22 @@ menu.style.left = "0px";
 }
 
 function searchStudent(){
+
 let input = document.getElementById("searchInput");
 let filter = input.value.toLowerCase();
+
 let rows = document.querySelectorAll("tbody tr");
 
-rows.forEach(row=>{
-let name = row.cells[0].innerText.toLowerCase();
+rows.forEach(function(row){
+
+let name = row.cells[1].innerText.toLowerCase();
 
 if(name.includes(filter)){
 row.style.display="";
 }else{
 row.style.display="none";
 }
+
 });
+
 }
