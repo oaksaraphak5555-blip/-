c52934fb6c556fda09ea55e87685874b7dccabb8
oaksaraphak5555-blip <oaksaraphak5555@gmail.com>
2026-03-06 17:00:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 function toggleMenu(){
-  document.getElementById("menu").classList.toggle("active");
+let menu = document.getElementById("menu");
+
+if(menu.style.left === "0px"){
+menu.style.left = "-200px";
+}else{
+menu.style.left = "0px";
+}
 }
 
 function searchStudent(){
-  let input = document.getElementById("searchInput").value.toLowerCase();
-  let rows = document.querySelectorAll("tbody tr");
+let input = document.getElementById("searchInput");
+let filter = input.value.toLowerCase();
+let rows = document.querySelectorAll("tbody tr");
 
-  rows.forEach(function(row){
-    if(row.textContent.toLowerCase().includes(input)){
-      row.style.display="";
-    }else{
-      row.style.display="none";
-    }
-  });
+rows.forEach(row=>{
+let name = row.cells[0].innerText.toLowerCase();
+
+if(name.includes(filter)){
+row.style.display="";
+}else{
+row.style.display="none";
+}
+});
 }
